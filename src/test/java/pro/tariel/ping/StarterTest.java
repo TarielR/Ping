b907cdd116bf58352536655a212c6176ae4083ef
Tariel <tar.iel.reg@gmail.com>
2020-04-24 13:12:26 -0400

    Readme.md added

---
+++ src/test/java/pro/tariel/ping/StarterTest.java
@@ -0,0 +1,11 @@
+package pro.tariel.ping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterTest {
+    @Test
+    public void contextLoads() {
+    }
+}
