70fc6a55d18cc764ab765c10ef1c661d153a6526
Tariel <tar.iel.reg@gmail.com>
2020-04-25 01:03:41 -0400

    Annotation @JsonInclude(JsonInclude.Include.NON_NULL) added

--- src/main/java/pro/tariel/ping/domain/person/Person.java
+++ src/main/java/pro/tariel/ping/domain/person/Person.java
@@ -1,5 +1,6 @@
 package pro.tariel.ping.domain.person;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Person {
     private Long id;
     private String name;
