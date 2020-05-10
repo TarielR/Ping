e93ba9939c099702dbe4c00d365968506c1548da
Tariel <tar.iel.reg@gmail.com>
2020-05-10 13:54:20 -0400

    Insert mock POST method for Person

--- src/main/java/pro/tariel/ping/controller/PersonController.java
+++ src/main/java/pro/tariel/ping/controller/PersonController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.tariel.ping.domain.person.PersonDto;
 import pro.tariel.ping.service.PersonService;
@@ -29,5 +30,10 @@ public class PersonController {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(service.getPersonById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/person", produces = "application/json")
+    public ResponseEntity<String> createPerson() {
+        return new ResponseEntity<>("Person is created successfully", HttpStatus.OK);
     }
 }
